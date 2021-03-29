bc53c3ff11022aa6fa7dd265ff1fcf8b9b891cbe
spencercjh <shouspencercjh@foxmail.com>
2021-03-29 21:48:13 +0800

    In order to be compatible with all template formats, method names won't be extracted, so test files won't be generated #11

--- problem_impl/java_leetcode_problem.py
+++ problem_impl/java_leetcode_problem.py
@@ -3,14 +3,6 @@ from leetcode_problem import LeetCodeProblem, GitHubFile
 
 class JavaLeetCodeProblem(LeetCodeProblem):
 
-    def extract_function_signature_from_snippet(self) -> str:
-        open_brace = self.code_snippet.index('class Solution {') + len('class Solution {')
-        return self.code_snippet[open_brace:self.code_snippet.rindex('\n')]
-
-    def extract_function_name_from_signature(self) -> str:
-        words = [words for words in self.function_signature.strip().split(' ') if words]
-        return words[2][:words[2].rindex('(')]
-
     def setup_source_file(self) -> GitHubFile:
         return GitHubFile(
             f"src/main/java/{self.file_user}/problems/{self.title_without_space}.java",
@@ -18,42 +10,17 @@ class JavaLeetCodeProblem(LeetCodeProblem):
             self.setup_java_source_file_content()
         )
 
+    def extract_class_docs(self):
+        if self.code_snippet.startswith('/**'):
+            docs = self.code_snippet[
+                   self.code_snippet.index('/**'):self.code_snippet.index('*/') + len('*/')] \
+                .replace('/**', f'/**\n * https://leetcode-cn.com/problems/{self.title_slug}/\n *') \
+                .replace("*/\n\n", f'* \n * @author {self.file_user}\n */')
+            self.code_snippet = self.code_snippet[self.code_snippet.index('*/') + len('*/'):]
+            return docs
+        else:
+            return f'/**\n * https://leetcode-cn.com/problems/{self.title_slug}/\n * \n * @author {self.file_user}\n */'
+
     def setup_java_source_file_content(self):
-        return f"""package {self.file_user}.problems;
 
-/**
- * https://leetcode-cn.com/problems/{self.title_slug}/
- *
- * @author {self.file_user}
- */
-public class {self.title_without_space} {{
-{self.function_signature}
-
-}}
-"""
-
-    def setup_test_file(self):
-        return GitHubFile(
-            f"src/test/java/{self.file_user}/problems/{self.title_without_space}Test.java",
-            f'{self.id}: {self.title_with_space} (Test)',
-            self.setup_java_test_file_content()
-        )
-
-    def setup_java_test_file_content(self):
-        return f"""package {self.file_user}.problems;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class {self.title_without_space}Test {{
-
-  private {self.title_without_space} solution = new {self.title_without_space}();
-  
-  @Test
-  void {self.function_name}() {{
-    
-  }}
-
-}}
-"""
+        return f'package {self.file_user}.problems;\n\n{self.class_docs}\n{self.code_snippet}'
